c00a5c644f9c219bff66888ac0e577aed8db53a2
Szurovecz János <szjani@szjani.hu>
2010-05-03 14:53:39 +0000

    Paginator adapter cache error with different tokens. Fixed.
    git-svn-id: http://zend-service-brightcove.googlecode.com/svn/trunk@37 c89d7ba7-3528-4f91-e2a3-bbede5390c7f

--- sample/application/modules/default/controllers/VideoController.php
+++ sample/application/modules/default/controllers/VideoController.php
@@ -3,20 +3,13 @@ class VideoController extends Zend_Controller_Action
 {
     public function findAllVideosAction()
     {
-    	try {
-	        $query = new ZendX_Service_Brightcove_Query_Read_FindAllVideos();
-	        $query->setVideoFields(new ZendX_Service_Brightcove_Set_VideoField(ZendX_Service_Brightcove_Enums_VideoFieldEnum::getConstants()));
-	        $paginator = new Zend_Paginator(new ZendX_Service_Brightcove_Paginator_ListAdapter($query));
-	        $paginator
-	            ->setCurrentPageNumber($this->_getParam('page', 1))
-	            ->setItemCountPerPage($this->_getParam('items', 10));
-	        $this->view->paginator = $paginator;
-	        $paginator->getCurrentItems();
-    	} catch (Exception $e) {
-    		var_dump(Zend_Service_Abstract::getHttpClient()->getLastRequest());
-    		var_dump($e);
-    		exit;
-    	}
+        $query = new ZendX_Service_Brightcove_Query_Read_FindAllVideos();
+        $query->setVideoFields(new ZendX_Service_Brightcove_Set_VideoField(ZendX_Service_Brightcove_Enums_VideoFieldEnum::getConstants()));
+        $paginator = new Zend_Paginator(new ZendX_Service_Brightcove_Paginator_ListAdapter($query));
+        $paginator
+            ->setCurrentPageNumber($this->_getParam('page', 1))
+            ->setItemCountPerPage($this->_getParam('items', 10));
+        $this->view->paginator = $paginator;
     }
     
     public function findVideosByIdsAction()
