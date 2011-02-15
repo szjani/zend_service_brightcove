a2efef41c99589523bd93b91580028c87dd1f1b8
Szurovecz János <szjani@szjani.hu>
2011-02-15 15:05:45 +0000

    unfiltered video by reference id
    git-svn-id: http://zend-service-brightcove.googlecode.com/svn/trunk@53 c89d7ba7-3528-4f91-e2a3-bbede5390c7f

---
+++ ZendX/Service/Brightcove/Query/Read/FindVideoByReferenceIdUnfiltered.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'ZendX/Service/Brightcove/Query/Read/VideoOne.php';
+
+/**
+ * @category   ZendX
+ * @package    ZendX_Service
+ * @subpackage Brightcove
+ * @author     szjani@szjani.hu
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class ZendX_Service_Brightcove_Query_Read_FindVideoByReferenceIdUnfiltered
+    extends ZendX_Service_Brightcove_Query_Read_VideoOne
+{
+    /**
+     * @param string $referenceId
+     */
+    public function  __construct($referenceId)
+    {
+        $this->setReferenceId($referenceId);
+    }
+
+    /**
+     * @see http://docs.brightcove.com/en/media/
+     */
+    public function getBrightcoveMethod()
+    {
+        return 'find_video_by_reference_id_unfiltered';
+    }
+
+    /**
+     * @param string $referenceId
+     * $return ZendX_Service_Brightcove_Query_FindVideoByReferenceId $this
+     */
+    public function setReferenceId($referenceId)
+    {
+        $this->setParam('reference_id', (string)$referenceId);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReferenceId()
+    {
+        return $this->getParam('reference_id');
+    }
+}
